7e2346ed7952a48db6f58f34de384e94c45517a9
Manash-epam04 <m_manashbandhubarik@epam.com>
2026-05-13 15:49:07 +0530

    public api destination added

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     return [
       {
         source: '/api/crypto/:path*',
-        destination: 'http://127.0.0.1:8000/api/v1/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`
+          : 'http://127.0.0.1:8000/api/v1/:path*',
       },
     ]
   },
